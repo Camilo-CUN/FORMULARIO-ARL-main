a0a76736f424822142d9424d69fd497700d46642
Camilo-CUN <Johan_trianaa@cun.edu.co>
2023-05-02 12:23:50 -0500

    Campos añadidos segun tipo de practica y todos los campos obligatorios

--- src/app/Modules/form/pages/form-page/form-page.component.ts
+++ src/app/Modules/form/pages/form-page/form-page.component.ts
@@ -31,6 +31,10 @@ export class FormPageComponent implements OnInit {
     NombreEstudiante:'',
     ProgramaAcademico:'',
     TipoPractica:'',
+    RutFile:'',
+    NombreEmprendimiento:'',
+    NitEmprendimiento:'',
+    CamaraComrcioFile:'',
     FechaNacimiento:'',
     EpsEstudiante:'',
     DocumentoEPSFile:'',
@@ -65,13 +69,29 @@ export class FormPageComponent implements OnInit {
     }
     if(this.formData.EmailEstudiante == "" || this.formData.ModalidadPractica == "" || this.formData.PeriodoAcademico == "" || this.formData.DocumentoIdentidadFile == ""
     ||this.formData.NumeroIdentifiacion == "" || this.formData.NombreEstudiante == "" || this.formData.ProgramaAcademico == "" || this.formData.TipoPractica == ""
-    ||this.formData.FechaNacimiento == ""){
+    ||this.formData.FechaNacimiento == "" || this.formData.EpsEstudiante == "" || this.formData.DocumentoEPSFile == "" || this.formData.NumeroTelEstudiante == "" || this.formData.CorreoInstitucional == ""
+    ||this.formData.NombreEmpresaPracticas == "" || this.formData.NitEmpresaPracticas == "" || this.formData.RiesgoEstudiante == "" || this.formData.NombrePersonaAcargoPractica == ""  || this.formData.TelefonoPersonasAcargo == "" || this.formData.EmailPersonaAcargoPractica == ""
+    ||this.formData.FechaInicioPractica == "" || this.formData.FechaTerminacionPractica == "" || this.formData.FechaTerminacionPractica == "" || this.formData.ActaInicioPractica == "" || this.formData.Regional == ""){
       Swal.fire({
         title: '¡Error!',
         text: 'Todos los campos son obligatorios',
         icon: 'warning'
       });
     }else return this.Enviar()
+
+    if(this.formData.ModalidadPractica == "Opcion1", this.formData.ModalidadPractica == "Opcion2"){
+      if(this.formData.EmailEstudiante == "" || this.formData.RutFile == "" || this.formData.NombreEmprendimiento == "" || this.formData.NitEmprendimiento =="" || this.formData.CamaraComrcioFile == "" || this.formData.PeriodoAcademico == "" || this.formData.DocumentoIdentidadFile == ""
+      ||this.formData.NumeroIdentifiacion == "" || this.formData.NombreEstudiante == "" || this.formData.ProgramaAcademico == "" || this.formData.TipoPractica == ""
+      ||this.formData.FechaNacimiento == "" || this.formData.EpsEstudiante == "" || this.formData.DocumentoEPSFile == "" || this.formData.NumeroTelEstudiante == "" || this.formData.CorreoInstitucional == ""
+      ||this.formData.NombreEmpresaPracticas == "" || this.formData.NitEmpresaPracticas == "" || this.formData.RiesgoEstudiante == "" || this.formData.NombrePersonaAcargoPractica == ""  || this.formData.TelefonoPersonasAcargo == "" || this.formData.EmailPersonaAcargoPractica == ""
+      ||this.formData.FechaInicioPractica == "" || this.formData.FechaTerminacionPractica == "" || this.formData.FechaTerminacionPractica == "" || this.formData.ActaInicioPractica == "" || this.formData.Regional == ""){
+        Swal.fire({
+          title: '¡Error!',
+          text: 'Todos los campos son obligatorios',
+          icon: 'warning'
+        });
+      }else return this.Enviar()
+    }
 
 
 
@@ -91,6 +111,10 @@ Enviar(){
     form.append('NombreEstudiante', this.formData.NombreEstudiante);
     form.append('ProgramaAcademico', this.formData.ProgramaAcademico);
     form.append('TipoPractica', this.formData.TipoPractica);
+    form.append('RutFile', this.formData.RutFile);
+    form.append('CamaraComercioFile', this.formData.CamaraComrcioFile);
+    form.append('NombreEmprendimiento', this.formData.NombreEmprendimiento);
+    form.append('NitEmprendimiento', this.formData.NitEmprendimiento);
     form.append('FechaNacimiento', this.formData.FechaNacimiento);
     form.append('EpsEstudiante', this.formData.EpsEstudiante);
     form.append('DocumentoEPSFile', this.formData.DocumentoEPSFile);
@@ -106,6 +130,7 @@ Enviar(){
     form.append('FechaTerminacionPractica', this.formData.FechaTerminacionPractica);
     form.append('ActaInicioPractica', this.formData.ActaInicioPractica);
     form.append('Regional', this.formData.Regional);
+    
     const formDataJson = JSON.stringify(this.formData);
     //console.log(formDataJson);
     const httpOptions = {
@@ -195,6 +220,28 @@ onFileChange(event: any) {
       if(reader.result){
         const base66 =reader.result.toString().split(',')[1];
         this.formData.ActaInicioPractica = base66;
+      }else{
+        console.error("Error De archivo")
+      }
+    };
+  }else if (event.target.name ==='RutFile'){
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      if(reader.result){
+        const base67 = reader.result.toString().split(',')[1];
+        this.formData.RutFile = base67;
+      }else{
+        console.error("Error De archivo")
+      }
+    };
+  }else if (event.target.name === 'CamaraComercioFile'){
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () =>{
+      if(reader.result){
+        const base68 = reader.result.toString().split(',')[1];
+        this.formData.CamaraComrcioFile = base68;
       }else{
         console.error("Error De archivo")
       }
